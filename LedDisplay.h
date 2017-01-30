008e36b609d4b656c1fe5f850aa2840c4975c39a
Aaron Miller <acmiller273@gmail.com>
2017-01-30 15:55:55 -0500

    Fixed unnecessarily large buffer

--- LedDisplay.h
+++ LedDisplay.h
@@ -93,9 +93,7 @@ class LedDisplay : public Print {
 																// be sent to the display by loadDotRegister()
 
   	int cursorPos;				// position of the cursor		
-//   	uint8_t dotRegister[40];    // the 320-bit dot register for a single 8 digit LED display
-// 	uint8_t dotRegister[80];    // the pair of 320-bit dot register for two 8 character LED displays
-   	uint8_t dotRegister[160];    // four 320-bit dot registers. 320 for each 8 character LED display 
+	uint8_t dotRegister[40];    // 5 bytes per character * up to 8 characters
 
 	// Define pins for the LED display:
 	uint8_t  dataPin;         	// connects to the display's data in
